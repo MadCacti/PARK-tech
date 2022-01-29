8e514a7e0dd776d78dc727a9f28af0cd9542f579
pauldipasquale <pauldipasquale756@gmail.com>
2022-01-28 16:53:56 -0800

    test for deployment

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import json
 
 # import app as app
-import requests
-from flask import render_template, request, Flask
+# import requests
+from flask import render_template, request
 from __init__ import app
 from newsapi.newsapi_client import NewsApiClient
 from crud.app_crud import app_crud
